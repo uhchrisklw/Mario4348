0e71da1140cf008d3308cd60da4289ba78ff0c2d
Lindsay Ortiz <Lindsay.ortiz@live.com>
2018-10-02 17:50:10 -0500

    Add title screen project (includes some assets). **Currently loads placeholder for world 1-1 load screen**

---
+++ Mario4348/Mario Title Screen/Assets/Scripts/BGMusicScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusicScript : MonoBehaviour {
+
+    public AudioSource bgMusicSource;
+    private bool audioBegin = false;
+
+    void Awake() {
+        if(!audioBegin) {
+            bgMusicSource.Play();
+            DontDestroyOnLoad(gameObject);
+            audioBegin = true;
+        }
+    }
+}
---
+++ Mario4348/Mario Title Screen/Assets/Scripts/CursorScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorScript : MonoBehaviour {
+
+    public GameObject startCursor;
+    public GameObject exitCursor;
+    public AudioSource soundSource;
+    public string nextSceneLoad;
+
+    private GameObject activeCursor;
+
+    void Start() {
+        activeCursor = startCursor;
+    }
+
+    void Update () {
+        if((Input.GetKeyDown("down") || Input.GetKeyDown("s")) && activeCursor == startCursor) {
+            soundSource.Play();
+
+            exitCursor.SetActive(true);
+            activeCursor = exitCursor;
+
+            startCursor.SetActive(false);
+        }
+
+        if ((Input.GetKeyDown("up") || Input.GetKeyDown("w")) && activeCursor == exitCursor) {
+            soundSource.Play();
+
+            startCursor.SetActive(true);
+            activeCursor = startCursor;
+
+            exitCursor.SetActive(false);
+        }
+
+        if(Input.GetKeyDown("space")) {
+            if (activeCursor == startCursor) {
+                SceneManager.LoadScene(nextSceneLoad);
+            }
+
+            if(activeCursor == exitCursor) {
+                Debug.Log("Exiting game...");
+                Application.Quit();
+            }
+        }
+	}
+}
